81c0c9681b4847d1c7822d9e22e6fdbcf3e5c068
sebas411 <sebasma411@gmail.com>
2025-11-16 11:40:44 -0600

    Added response code 200

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut response = String::new();
+                response.push_str("HTTP/1.1 200 OK\r\n");
+                response.push_str("\r\n");
+                stream.write_all(response.as_bytes()).expect("Unable to write response to stream.");
             }
             Err(e) => {
                 println!("error: {}", e);
