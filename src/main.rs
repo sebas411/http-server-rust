60ee71d55cc99c60d9755843afb9e1b81607f00c
sebas411 <sebasma411@gmail.com>
2026-01-18 11:22:15 -0600

    implement /echo/<str> endpoint

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,21 @@ fn main() -> Result<()> {
                 let split_buffer = buffer_string.split("\r\n").collect::<Vec<_>>();
                 let req = split_buffer[0];
                 let path = req.split(' ').nth(1).unwrap_or("");
+                let mut content = String::new();
+                let mut content_headers = String::new();
                 let status_code;
-                if path == "/" {
-                    status_code = 200;
-                } else {
-                    status_code = 404;
+                match path {
+                    "/" => {
+                        status_code = 200;
+                    },
+                    s if s.len() > 6 && &s[..6] == "/echo/" => {
+                        status_code = 200;
+                        let echo_text = &s[6..];
+                        content = echo_text.to_string();
+                    },
+                    _ => {
+                        status_code = 404;
+                    },
                 }
                 let status_text;
                 if status_code == 404 {
@@ -28,8 +38,11 @@ fn main() -> Result<()> {
                 } else {
                     status_text = "OK".to_string();
                 }
+                if !content.is_empty() {
+                    content_headers = format!("Content-Type: text/plain\r\nContent-Length: {}\r\n", content.len());
+                }
                 let mut response = String::new();
-                response.push_str(&format!("HTTP/1.1 {} {}\r\n", status_code, status_text));
+                response.push_str(&format!("HTTP/1.1 {} {}\r\n{}\r\n{}", status_code, status_text, content_headers, content));
                 response.push_str("\r\n");
                 stream.write_all(response.as_bytes()).expect("Unable to write response to stream.");
             }
